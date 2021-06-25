60bc5d0b25eea1892db0072721be1df541e6a293
Miriama Svítková <miri52@users.noreply.github.com>
2021-06-25 18:22:48 +0100

    Add a new post
    - POST, options
    - show new post

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+const postForm = document.getElementById("post-form")
+const blogList = document.getElementById("blog-list")
+
+let html = ''
+
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(res => res.json())
     .then(data => {
         const postsArr = data.slice(0, 5)
-        let html = ""
         for (let post of postsArr) {
             html += `
                 <h3>${post.title}</h3>
@@ -10,5 +14,40 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
                 <hr />
             `
         }
-        document.getElementById("blog-list").innerHTML = html
+        blogList.innerHTML = html
     })
+
+
+const submitPost = (e) => {
+    e.preventDefault();
+    const title = document.getElementById("post-title").value;
+    const body = document.getElementById("post-body").value;
+
+    const postData = {
+            title,
+            body
+        }
+    
+    fetch('https://apis.scrimba.com/jsonplaceholder/posts', {
+        method: "POST",
+        body: JSON.stringify(postData),
+        headers:  {
+            "Content-Type": "application/json"
+    }
+    })
+    .then(res => res.json())
+    .then(data => {
+       const {title, body} = data;
+       let newPost = ''
+       newPost += `
+            <h3>${title}</h3>
+            <p>${body}</p>
+            <hr />
+       ` 
+       blogList.insertAdjacentHTML('afterbegin', newPost)    
+    })
+    
+
+}    
+
+postForm.addEventListener('submit', submitPost)    
